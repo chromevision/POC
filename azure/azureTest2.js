9a5d929506b1d90c35fd66c7ed0d17722a588b91
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-28 19:13:36 -0500

    imgur api in progress

--- azure/azureTest2.js
+++ azure/azureTest2.js
@@ -1,12 +1,19 @@
 var express = require('express');
 const request = require('request');
 var bodyParser = require('body-parser');
+const axios = require('axios');
 
 var app = express();
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+app.use(
+  bodyParser.raw({
+    type: 'application/octet-stream',
+    limit: '10mb',
+  })
+);
 
 // Replace <Subscription Key> with your valid subscription key.
-const subscriptionKey = '07463fe19ab1489aa9676f89bcb74fe3';
 
 // You must use the same location in your REST call as you used to get your
 // subscription keys. For example, if you got your subscription keys from
@@ -15,39 +22,65 @@ const uriBase = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect';
 
 // Request parameters.
 var params = {
-	returnFaceId: 'true',
-	returnFaceLandmarks: 'false',
-	returnFaceAttributes: 'emotion'
+  returnFaceId: 'true',
+  returnFaceLandmarks: 'false',
+  returnFaceAttributes: 'emotion',
 };
 
 app.post('/api/webcam', async (req, res, next) => {
-	console.log('post receivedXHR', req.body);
-	console.log('params', req.params);
-	res.send('resp');
-	// 	const options = {
-	// 		uri: uriBase,
-	// 		qs: params,
-	// 		body: '{"url": ' + '"' + image + '"}',
-	// 		headers: {
-	// 			'Content-Type': 'application/octet-stream',
-	// 			'Ocp-Apim-Subscription-Key': subscriptionKey
-	// 		}
-	// 	};
-	// 	try {
-	// 		request.post(options, (error, response, body) => {
-	// 			if (error) {
-	// 				console.log('Error: ', error);
-	// 				return;
-	// 			}
-	// 			let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-	// 			console.log('JSON Response\n');
-	// 			console.log(jsonResponse);
-	// 		});
-	// 	} catch (error) {}
+  try {
+    const base64Image = Buffer.from(req.body).toString('base64');
+    // const imgurRes = await axios.post('https://api.imgur.com/3/image', {
+    //   image: base64Image,
+    //   type: 'base64',
+    //   headers: {
+    //     Authorization: `Client-ID 5feca055666f551`,
+    //   },
+    // });
+
+    const imgurRes = await axios.post(
+      'https://api.imgur.com/3/image',
+      {
+				image: `data:image/png;base64 ${base64Image}`,
+        album: 'VEHhzmA'
+      },
+      {
+        headers: {
+					'content-type':
+            'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW',
+          Authorization: 'Client-ID 5feca055666f551',
+        },
+      }
+    );
+
+    console.log('imgur respose', imgurRes);
+		// const imageLink = imgurRes.data.link;
+		const azureKey = '07463fe19ab1489aa9676f89bcb74fe3';
+    const options = {
+      uri: uriBase,
+      qs: params,
+      // body: ,
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': azureKey,
+      },
+    };
+    request.post(options, (error, response, body) => {
+      if (error) {
+        console.log('Error: ', error);
+        return;
+      }
+      let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+      console.log('JSON response:', jsonResponse);
+    });
+    // res.send('resp');
+  } catch (error) {
+	    next(error);
+  }
 });
 
 app.listen(3000, function() {
-	console.log('Listening on port 3000!');
+  console.log('Listening on port 3000!');
 });
 
 module.exports = app;
--- extension/options.js
+++ extension/options.js
@@ -24,92 +24,71 @@ const video = document.getElementById('live-video');
 // video.onClick = () => snapshot();
 
 navigator.mediaDevices
-	.getUserMedia({ video: true, audio: false })
-	.then(function(stream) {
-		video.srcObject = stream;
-		video.play();
-	})
-	.catch(function(err) {
-		console.log('An error occurred! ' + err);
-	});
+  .getUserMedia({ video: true, audio: false })
+  .then(function(stream) {
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch(function(err) {
+    console.log('An error occurred! ' + err);
+  });
 
 const canvas = document.getElementById('canvas');
 const photo = document.getElementById('photo');
 
-function doTheThing(imgData) {
-	console.log('dothething');
-	console.log(imgData);
-	// var xhr = new XMLHttpRequest();
+async function doTheThing(imgData) {
+  console.log(imgData);
+  console.log('dothething');
+  await fetch('http://localhost:3000/api/webcam', {
+    method: 'POST',
+    body: imgData,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/octet-stream',
+    },
+  });
+}
 
-	// xhr.onload = () => {
-	// 	if (xhr.status >= 200 && xhr.status < 300) {
-	// 		console.log('success!', xhr);
-	// 	} else {
-	// 		console.log('ugh');
-	// 	}
-	// 	console.log('every single time');
-	// };
-
-	// Request parameters.
-	// var params = {
-	// 	returnFaceId: true,
-	// 	returnFaceLandmarks: false,
-	// 	returnFaceAttributes: 'emotion'
-	// };
-
-	// const url = 'http://localhost:3000/api/webcam';
-
-	// fetch(url, {
-	// 	method: 'POST',
-	//   body: imgData,
-
-	// });
-	const body = new FormData();
-	body.append('image', imgData);
-	// const body = imgData;
-	const xhr = new XMLHttpRequest();
-	xhr.open('POST', 'http://localhost:3000/api/webcam');
-	// xhr.setRequestHeader('Content-Type', 'application/octect-stream');
-	// xhr.setRequestHeader('Content-Type', 'application/json');
-
-	xhr.send(body);
-
-	// xhr.open('POST', `http://localhost:3000/api/webcam`);
-	// xhr.setRequestHeader('Content-Type', 'application/json');
-	// xhr.setRequestHeader(
-	// 	'Ocp-Apim-Subscription-Key',
-	// 	'07463fe19ab1489aa9676f89bcb74fe3'
-	// );
-
-	// xhr.send(new Blob(imgData));
+function dataURLtoBlob(dataurl) {
+  var arr = dataurl.split(','),
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new Blob([u8arr], { type: mime });
 }
 
 function takePicture() {
-	var context = canvas.getContext('2d');
-	var width = canvas.width;
-	var height = canvas.height;
-	if (width && height) {
-		canvas.width = width;
-		canvas.height = height;
-		context.drawImage(video, 0, 0, width, height);
+  var context = canvas.getContext('2d');
+  var width = canvas.width;
+  var height = canvas.height;
+  if (width && height) {
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(video, 0, 0, width, height);
 
 		var data = canvas.toDataURL('image/png');
-		doTheThing(data);
+		console.log(data)
+    // var toSend = dataURLtoBlob(data);
+    doTheThing(data);
 
-		// var data =
-		// 	canvas.toBlob(function(blob) {
-		// 		let blobby = URL.createObjectURL(blob);
-		// 		console.log(blobby);
-		// 		// 	console.log(link.href); // this line should be here
-		// 	})
-		// 	// , 'image/png')
-		// );
-		// photo.setAttribute('src', data);
-	} else {
-		// clearphoto();
-	}
+    // var data =
+    // 	canvas.toBlob(function(blob) {
+    // 		let blobby = URL.createObjectURL(blob);
+    // 		console.log(blobby);
+    // 		// 	console.log(link.href); // this line should be here
+    // 	})
+    // 	// , 'image/png')
+    // );
+    // photo.setAttribute('src', data);
+  } else {
+    // clearphoto();
+  }
 }
 
 setInterval(function() {
-	takePicture();
+  takePicture();
 }, 5000);
