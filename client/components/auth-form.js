e5671addc2ad3595f1835b36b91a143227f658da
Joseph Lionarons <lionsamurai@gmail.com>
2019-02-01 15:56:08 -0500

    navbar styled

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -12,67 +12,70 @@ import { auth } from '../store';
 // testing();
 
 const AuthForm = props => {
-	const { name, displayName, handleSubmit, error } = props;
+  const { name, displayName, handleSubmit, error } = props;
 
-	return (
-		<div>
-			<form onSubmit={handleSubmit} name={name}>
-				<div>
-					<label htmlFor="email">
-						<small>Email</small>
-					</label>
-					<input name="email" type="text" />
-				</div>
-				<div>
-					<label htmlFor="password">
-						<small>Password</small>
-					</label>
-					<input name="password" type="password" />
-				</div>
-				<div>
-					<button type="submit">{displayName}</button>
-				</div>
-				{error && error.response && <div> {error.response.data} </div>}
-			</form>
-			<a href="/auth/google">{displayName} with Google</a>
-		</div>
-	);
+  return (
+    <div id="auth-form" className="sub-nav">
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">{displayName}</button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+      <div id="auth-google">
+        <p>- OR -</p>
+        <a href="/auth/google">{displayName} with Google</a>
+      </div>
+    </div>
+  );
 };
 
 const mapLogin = state => {
-	return {
-		name: 'login',
-		displayName: 'Login',
-		error: state.user.error
-	};
+  return {
+    name: 'login',
+    displayName: 'Login',
+    error: state.user.error,
+  };
 };
 
 const mapSignup = state => {
-	return {
-		name: 'signup',
-		displayName: 'Sign Up',
-		error: state.user.error
-	};
+  return {
+    name: 'signup',
+    displayName: 'Sign Up',
+    error: state.user.error,
+  };
 };
 
 const mapDispatch = dispatch => {
-	return {
-		handleSubmit(evt) {
-			evt.preventDefault();
-			const formName = evt.target.name;
-			const email = evt.target.email.value;
-			const password = evt.target.password.value;
-			dispatch(auth(email, password, formName));
-		}
-	};
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const email = evt.target.email.value;
+      const password = evt.target.password.value;
+      dispatch(auth(email, password, formName));
+    },
+  };
 };
 
 export const Login = connect(mapLogin, mapDispatch)(AuthForm);
 export const Signup = connect(mapSignup, mapDispatch)(AuthForm);
 
 AuthForm.propTypes = {
-	name: PropTypes.string.isRequired,
-	displayName: PropTypes.string.isRequired,
-	handleSubmit: PropTypes.func.isRequired,
-	error: PropTypes.object
+  name: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.object,
 };
