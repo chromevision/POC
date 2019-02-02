ffb0c780153332040117995172491d1bcb8538c7
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-02-02 18:09:51 -0500

    real data for emotion trends on web

---
+++ client/store/emotions.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const GET_ALL_EMOTIONS = 'GET_ALL_EMOTIONS';
+
+const defaultAllEmotion = [];
+
+const getAllEmotions = (emotions) => {
+  return {
+    type: GET_ALL_EMOTIONS,
+    emotions
+  };
+};
+
+export const getAllEmotionsThunk = (id) => async dispatch => {
+  try {
+    const emotions = await axios.get(`/api/users/${id}/emotions`);
+    dispatch(getAllEmotions(emotions.data));
+
+  } catch (error) {
+    console.log(error);
+  };
+};
+
+
+export default function(state = defaultAllEmotion, action) {
+  switch (action.type) {
+    case GET_ALL_EMOTIONS:
+      return action.emotions;
+    default:
+      return state;
+  }
+}
