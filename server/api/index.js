23a67b857597934ab6f2e537fec73198c12e2d98
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-02-05 09:07:22 -0500

    webapp protection done

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+
 router.use('/users', require('./users'));
 router.use('/emotions', require('./emotions'));
 router.use('/webcam', require('./webcam'));
--- server/api/users.js
+++ server/api/users.js
@@ -3,8 +3,13 @@ const { User, Emotion } = require('../db/models');
 
 router.get('/', async (req, res, next) => {
   try {
-    const userList = await User.findAll();
-    res.send(userList);
+    if(!req.user || !req.user.admin){
+      res.send("You're not authorized!");
+    }
+    else{
+      const userList = await User.findAll();
+      res.send(userList);
+    }
   } catch (error) {
     next(error);
   }
@@ -12,8 +17,13 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const thisUser = await User.findById(req.params.id);
-    res.send(thisUser);
+    if(!req.user|| !req.user.admin){
+      res.send("You're not authorized!");
+    }
+    else{
+      const thisUser = await User.findById(req.params.id);
+      res.send(thisUser);
+    }
   } catch (error) {
     next(error);
   }
@@ -21,13 +31,18 @@ router.get('/:id', async (req, res, next) => {
 
 router.get('/:id/emotions', async (req, res, next) => {
   try {
-    const userEmotions = await Emotion.findAll({
-      where: {
-        userId: req.params.id,
-      },
-      include: [{ all: true }],
-    });
-    res.send(userEmotions);
+    if(!req.user){
+      res.send("You're not authorized!");
+    }
+    else{
+      const userEmotions = await Emotion.findAll({
+        where: {
+          userId: req.params.id,
+        },
+        include: [{ all: true }],
+      });
+      res.send(userEmotions);
+    }
   } catch (error) {
     next(error);
   }
