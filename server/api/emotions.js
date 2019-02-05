a5c5731558d149c397a9ed2940ff068d00ac54b6
sglord <lordstepheng@gmail.com>
2019-02-05 15:36:18 -0500

    master merging

--- server/api/emotions.js
+++ server/api/emotions.js
@@ -2,34 +2,34 @@ const router = require('express').Router();
 const { Emotion } = require('../db/models');
 
 router.get('/', async (req, res, next) => {
-  try {
-    if (!req.user || !req.user.admin) {
-      res.send("You're not authorized!");
-    } else {
-      const allEmotions = await Emotion.findAll();
-      res.send(allEmotions);
-    }
-  } catch (error) {
-    next(error);
-  }
+	try {
+		if (!req.user || !req.user.admin) {
+			res.send("You're not authorized!");
+		} else {
+			const allEmotions = await Emotion.findAll();
+			res.send(allEmotions);
+		}
+	} catch (error) {
+		next(error);
+	}
 });
 
 router.get('/:id', async (req, res, next) => {
-  try {
-    if (!req.user || !req.user.admin) {
-        res.send("You're not authorized!");
-    } else {
-      const userEmotions = await Emotion.findAll({
-        where: {
-          userTokenId: req.params.id,
-        },
-        include: [{ all: true }],
-      });
-      res.send(userEmotions);
-    }
-  } catch (error) {
-    next(error);
-  }
+	try {
+		if (!req.user || !req.user.admin) {
+			res.send("You're not authorized!");
+		} else {
+			const userEmotions = await Emotion.findAll({
+				where: {
+					userTokenId: req.params.id
+				},
+				include: [{ all: true }]
+			});
+			res.send(userEmotions);
+		}
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
