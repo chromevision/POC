04740479141322286a05ae2a11720143d0f14130
Joseph Lionarons <lionsamurai@gmail.com>
2019-02-06 08:59:09 -0500

    removed extraneous HTML elements

--- client/components/mainpage.js
+++ client/components/mainpage.js
@@ -2,45 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MainPage = () => {
-	return (
-		<div className="sub-nav centered enlarged-font wrap-container">
-			<div className="container main-container">
-				<h1>Welcome to the ChromeVision Web-App!</h1>
-				<p>
-					<strong>ChromeVision</strong> is a computer vision application that
-					uses your built-in webcam to monitor and perform sentiment analysis on
-					your facial expressions as you browse the web. This data is aggregated
-					and displayed through easily-readable visualizations that allow you to
-					better understand your personal browsing habits.
-				</p>
+  return (
+    <div className="sub-nav centered enlarged-font wrap-container">
+      <div className="container main-container">
+        <h1>Welcome to the ChromeVision Web-App!</h1>
+        <p>
+          <strong>ChromeVision</strong> is a computer vision application that
+          uses your built-in webcam to monitor and perform sentiment analysis on
+          your facial expressions as you browse the web. This data is aggregated
+          and displayed through easily-readable visualizations that allow you to
+          better understand your personal browsing habits.
+        </p>
 
-				<p>
-					If you are a <strong>new user</strong>, you must first{' '}
-					<Link to="/signup">
-						create a <strong>ChromeVision</strong> account,
-					</Link>{' '}
-					then install and log in with our companion Chrome extension.
-				</p>
+        <p>
+          If you are a <strong>new user</strong>, you must first{' '}
+          <Link to="/signup">
+            create a <strong>ChromeVision</strong> account,
+          </Link>{' '}
+          then install and log in with our companion Chrome extension.
+        </p>
 
-				<p>
-					If you are an <strong>existing user</strong>, please{' '}
-					<Link to="/login">sign in </Link>
-					to your existing account to view your data.
-				</p>
+        <p>
+          If you are an <strong>existing user</strong>, please{' '}
+          <Link to="/login">sign in </Link>
+          to your existing account to view your data.
+        </p>
 
-				<h2>Privacy</h2>
-				<p>
-					At <strong>ChromeVision</strong>, we value the importance of
-					maintaining your privacy. To this end, we keep{' '}
-					<strong>no record</strong> of any image or video data that is
-					collected from your webcam. We also claim{' '}
-					<strong>no ownership</strong> of your personal emotional data.
-				</p>
+        <h2>Privacy</h2>
+        <p>
+          At <strong>ChromeVision</strong>, we value the importance of
+          maintaining your privacy. To this end, we keep{' '}
+          <strong>no record</strong> of any image or video data that is
+          collected from your webcam. We also claim{' '}
+          <strong>no ownership</strong> of your personal emotional data.
+        </p>
 
-				<br />
-			</div>
-		</div>
-	);
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
