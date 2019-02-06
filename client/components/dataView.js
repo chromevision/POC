582c93af000a01e1a4ccd5e73686e259b6337dd3
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-02-06 15:52:11 -0500

    Header for data view

--- client/components/dataView.js
+++ client/components/dataView.js
@@ -8,48 +8,51 @@ class Dataview extends Component {
     this.props.getAllEmotions(this.props.User);
   }
   render() {
-    console.log(this.props.Emotions);
-    console.log(this.props.User);
     return (
-      <Table celled>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>URL</Table.HeaderCell>
-            <Table.HeaderCell>Anger Score</Table.HeaderCell>
-            <Table.HeaderCell>Contempt Score</Table.HeaderCell>
-            <Table.HeaderCell>Disgust Score</Table.HeaderCell>
-            <Table.HeaderCell>Fear Score</Table.HeaderCell>
-            <Table.HeaderCell>Happiness Score</Table.HeaderCell>
-            <Table.HeaderCell>Neutral Score</Table.HeaderCell>
-            <Table.HeaderCell>Sadness Score</Table.HeaderCell>
-            <Table.HeaderCell>Suprise Score</Table.HeaderCell>
-            <Table.HeaderCell>Date</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {this.props.Emotions &&
-            this.props.Emotions.map(emotion => {
-              const dateString = String(new Date(emotion.createdAt)).slice(
-                0,
-                24
-              );
-              return (
-                <Table.Row key={emotion.id}>
-                  <Table.Cell>{emotion.url}</Table.Cell>
-                  <Table.Cell>{emotion.anger}</Table.Cell>
-                  <Table.Cell>{emotion.contempt}</Table.Cell>
-                  <Table.Cell>{emotion.disgust}</Table.Cell>
-                  <Table.Cell>{emotion.fear}</Table.Cell>
-                  <Table.Cell>{emotion.happiness}</Table.Cell>
-                  <Table.Cell>{emotion.neutral}</Table.Cell>
-                  <Table.Cell>{emotion.sadness}</Table.Cell>
-                  <Table.Cell>{emotion.surprise}</Table.Cell>
-                  <Table.Cell>{dateString}</Table.Cell>
-                </Table.Row>
-              );
-            })}
-        </Table.Body>
-      </Table>
+      <div>
+        <div className="sub-nav centered enlarged-font wrap-container">
+          We aim for full user <strong>transparency</strong>! Here is all the data we have associated with {this.props.Email}:
+        </div>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>URL</Table.HeaderCell>
+              <Table.HeaderCell>Anger Score</Table.HeaderCell>
+              <Table.HeaderCell>Contempt Score</Table.HeaderCell>
+              <Table.HeaderCell>Disgust Score</Table.HeaderCell>
+              <Table.HeaderCell>Fear Score</Table.HeaderCell>
+              <Table.HeaderCell>Happiness Score</Table.HeaderCell>
+              <Table.HeaderCell>Neutral Score</Table.HeaderCell>
+              <Table.HeaderCell>Sadness Score</Table.HeaderCell>
+              <Table.HeaderCell>Suprise Score</Table.HeaderCell>
+              <Table.HeaderCell>Date</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.props.Emotions &&
+              this.props.Emotions.map(emotion => {
+                const dateString = String(new Date(emotion.createdAt)).slice(
+                  0,
+                  24
+                );
+                return (
+                  <Table.Row key={emotion.id}>
+                    <Table.Cell>{emotion.url}</Table.Cell>
+                    <Table.Cell>{emotion.anger}</Table.Cell>
+                    <Table.Cell>{emotion.contempt}</Table.Cell>
+                    <Table.Cell>{emotion.disgust}</Table.Cell>
+                    <Table.Cell>{emotion.fear}</Table.Cell>
+                    <Table.Cell>{emotion.happiness}</Table.Cell>
+                    <Table.Cell>{emotion.neutral}</Table.Cell>
+                    <Table.Cell>{emotion.sadness}</Table.Cell>
+                    <Table.Cell>{emotion.surprise}</Table.Cell>
+                    <Table.Cell>{dateString}</Table.Cell>
+                  </Table.Row>
+                );
+              })}
+          </Table.Body>
+        </Table>
+      </div>
     );
   }
 }
